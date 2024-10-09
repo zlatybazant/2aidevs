f36c709193b69d26347b3c423c109508608c6133
zlatybazant <jakub@kieryk.me>
2024-10-09 12:33:23 +0000

    adding final search.py

--- search.py
+++ search.py
@@ -4,26 +4,19 @@ from env import OPENAI_API_KEY
 from requests.exceptions import RequestException
 from docker_manager import ComposeManager
 from qdrant_client.models import Distance, VectorParams, PointStruct
-
-#def get_article_content(url, max_retries=5, backoff_factor=1):
-#    headers = { 'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/119.0' }
-#
-#    for attempt in range(max_retries):
-#        try:
-#            response = request.get(url, headers=headers, timeout=10)
-#            response.raise_for_status()
-#            return response.text
-#        except RequestException as e:
-#            print(f"Attempt {attempt + 1} failed: {e}")
-#            if attempt + 1 == max_retries:
-#                raise
-#            time.sleep(backoff_factor * (2 ** attempt))
-
+from concurrent.futures import ThreadPoolExecutor
+from uuid import uuid4
+import logging
 
 taskName = 'search'
 client = OpenAI(api_key=OPENAI_API_KEY)
 file_remote = 'https://unknow.news/archiwum.json'
 file_local = 'data/archiwum.json'
+BATCH_SIZE = 10
+PROGRESS_FILE = 'batch_progress.txt'
+LIMIT_VECTORS = 2000
+qclient = qdrant_client.QdrantClient(url='http://localhost:6333')
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
 def get_remote_file():
     if not os.path.exists(file_local):
@@ -35,61 +28,141 @@ def get_remote_file():
     else:
         print(f"File {file_local} already exists, skipping download")
 
-def connect_qdrant_collection(url='http://localhost:6333'):
+def connect_qdrant_collection():
     
-    client = qdrant_client.QdrantClient(url=url)
     try:
-        collection = client.get_collection("unknown_search")
-        print("qdrant: Collection 'unknown_search' exists, skipping creation")
+        collection = qclient.get_collection("new_collection")
+        print("qdrant: Collection 'new_collection' exists, skipping creation")
+        return collection
     except qdrant_client.http.exceptions.UnexpectedResponse:
 
-        print("qdrant: Collection 'unknown_search' does not exist, about to create it")
-        collection = client.create_collection(
-            collection_name="unknown_search",
-            vectors_config=VectorParams(size=1536, distance=Distance.COSINE)
-        )
+        print("qdrant: Collection 'new_collection' does not exist, about to create it")
+        collection = qclient.create_collection(
+            collection_name="new_collection",
+            vectors_config=VectorParams(size=1536, distance=Distance.COSINE))
+
+def save_progress(count):
+    with open(PROGRESS_FILE, 'w') as f:
+        f.write(str(count))
+
+def load_progress():
+    if os.path.exists(PROGRESS_FILE):
+        with open(PROGRESS_FILE, 'r') as f:
+            return int(f.read())
+    return 0
+
+def get_embedding(text, model="text-embedding-ada-002"):
+    text = text.replace("\n", " ")
+    return client.embeddings.create(input=[text], model=model).data[0].embedding
+
+
+def retry_logic(text, attemps=5, model="text-embedding-ada-002"):
+    delay = 1
+
+    for attempt in range(attemps):
+        try:
+            embedding = get_embedding(text, model=model)
+            return embedding
+        except Exception as e:
+            print(f"Error: Retry {i+1}/{attempts} failed: {e}")
+            time.sleep(delay)
+            delay *= 2
+    print("Max retries exceeded")
+    return None
+
+def get_openai_embeddings(items, model="text-embedding-ada-002"):
+    embeddings = []
+    for item in items:
+        text = item['title'].replace("\n", " ")
+        print(f"Getting embedding for item: {item['title']}")
+
+        embedding = retry_logic(text, model=model)
+        if embedding is None:
+            print(f"Skipping item due to failure: {item['title']}")
+            embeddings.appedn(None)
+        else:
+            embeddings.append(embedding)
+    return embeddings
+
 
 def main():
 
+    compose_manager = ComposeManager()
     compose_manager.stop_services()
-    #compose_manager = ComposeManager()
     compose_manager.start_services()
     compose_manager.status()
 
     try:
         token = taskResponse.get_task_token(taskName)
         print(f"Token dla zadania '{taskName}' to: {token}")
-        data = taskResponse.get_json(token)
-        print(f"Dane dla zadania '{taskName}' to: {data}")
+        task  = taskResponse.get_json(token)
+        print(f"Dane dla zadania '{taskName}' to: {task}")
 
         get_remote_file()
 
-        connect_qdrant_collection()
+        collection_info = connect_qdrant_collection()
+
+        cnt = load_progress()
+
+        if collection_info.vectors_count >= LIMIT_VECTORS:
+            print(f"qdrant: Collection already has {LIMIT_VECTORS} vectors, no need to add more.")
+            return
+
+        with open(file_local, 'r') as f:
+            data = json.load(f)
 
 
-        #print(articleLink)
-        #print(question)
+        print(f"Loaded {len(data)} items from {file_local}")
 
-        #articleContent = requests.get(articleLink)
-        #print("doc retreived")
-	##body = json.loads(request.text)
+        data = data[collection_info.vectors_count:] if cnt < collection_info.vectors_count else data[cnt:]
 
-        #completion = client.chat.completions.create(
-	#    model="gpt-3.5-turbo",
-	#    messages=[
-	#    {"role": "system", "content": f"Odpowiedz po polsku na pytania zwiazane z tym kontekstem. Odpowiadaj zwięźle. Ogranicz długość odpowiedzi do 200 znaków:\n\n {articleContent}"},
-	#    {"role": "user", "content": f"Question is: {question}"}
-	#    ]
-        #)
+        batch = []
+        for idx, item in enumerate(data, start=cnt):
+            batch.append(item)
+            print(f"Adding item {idx} to batch: {item['title']}")
 
-        #messageOutput = completion.choices[0].message.content
-	###parsed_content = json.loads(message_content)
-        #print(f"OAI response: {messageOutput}")
-        #answer={ 'answer': messageOutput }
-        #print(f"answer: {answer} ")
-        #response = taskResponse.post_answer(token, answer)
-        #print(response.status_code)
-        #print(response.text)
+            if len(batch) == BATCH_SIZE:
+
+                print(f"Processing batch of {BATCH_SIZE} items, starting at index {idx - BATCH_SIZE + 1}")
+
+                embeddings_batch = get_openai_embeddings(batch)
+
+                points = []
+                for i, (item, embedding) in enumerate(zip(batch, embeddings_batch)):
+                    if embedding is None:
+                        logging.warning(f"Skipping item {idx - len(batch) + i + 1} due to missing embedding")
+                        continue
+                    item['uuid'] = str(uuid4())
+                    points.append(PointStruct(id=idx - len(batch) + i + 1, vector=embedding, payload=item))
+                if points:
+                    result = qclient.upsert(collection_name="new_collection", points=points)
+                    #logging.debug(f"qdrant: Inserted batch, Result: {result}")
+        
+                save_progress(idx + 1)
+                batch = []
+
+            if idx >= LIMIT_VECTORS:
+                print(f"qdrant: Limit of {LIMIT_VECTORS} vectors being processed reached, stopping. Check qdrantDB total points value")
+                break
+            
+        print(f"qdrant: Finished processing up to {LIMIT_VECTORS} vectors from data in given file.")
+
+        
+        question  = task.get('question').strip().lower()
+        print(f"task question: {question}")
+        search = get_embedding(question)
+
+        finds = qclient.search(collection_name="new_collection", query_vector=search, limit=1)
+        for find in finds:
+            print(f"Found result: {find.payload}")
+        print(f"qdrant: closest find: {finds[0].payload['title']}, url {finds[0].payload['url']}")
+        url = finds[0].payload['url']
+        
+        answer={ 'answer': url }
+        print(f"answer: {answer} ")
+        response = taskResponse.post_answer(token, answer)
+        print(response.status_code)
+        print(response.text)
 
     except Exception as e:
         print(f"Error: {e}")
